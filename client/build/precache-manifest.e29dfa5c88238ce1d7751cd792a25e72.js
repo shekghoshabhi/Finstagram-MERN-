2c88b7be0ad095259c22e02fcc69fe9f76bdfe70
Abhishek ghosh <shekghoshabhi@gmail.com>
2021-08-27 02:21:09 +0530

    Add files via upload

---
+++ client/build/precache-manifest.e29dfa5c88238ce1d7751cd792a25e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad43776fb3062ab7af67d7730449631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e19e91313b60ee87a31",
+    "url": "/static/css/main.5062ff3c.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e19e91313b60ee87a31",
+    "url": "/static/js/main.ffcda5cf.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+    // MONGOURI:process.env.MOGOURI,
+    JWT_SECRET: 'secret',
+    // SENDGRID_API:process.env.SENDGRID_API,
+    // EMAIL:process.env.EMAIL
+}
